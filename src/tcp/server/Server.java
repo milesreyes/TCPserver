700d9669aeb5373e79d4530e6e2997fe20ece1d6
c <techrockman@gmail>
2016-01-16 12:06:50 -0800

    initial commit

---
+++ src/tcp/server/Server.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcp.server;
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author milesreyes
+ */
+public class Server {
+    public static void main(String argv[]) throws Exception
+            { 
+                String clientSentence;
+                String capitalizedSentence;         
+                ServerSocket welcomeSocket = new ServerSocket(6789);
+                 while(true) {             
+                     Socket connectionSocket = welcomeSocket.accept();            
+                     BufferedReader inFromClient =                
+                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));             
+                     DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());            
+                     clientSentence = inFromClient.readLine();             
+                     System.out.println("Received: " + clientSentence);             
+                     capitalizedSentence = clientSentence.toUpperCase() + '\n';             
+                     outToClient.writeBytes(capitalizedSentence);
+                  } 
+            }  
+
+
+    
+}
+
